e2cf9d18963870945efa7fc337d71e7f481d5ab7
Marcus Nilsson <marcus.niloen.com>
2017-03-11 16:14:10 +0100

    Used lokbok

--- src/test/java/com/niloen/spring/data/google/datastore/repository/GoogleDatastoreRepositoryIntegrationTests.java
+++ src/test/java/com/niloen/spring/data/google/datastore/repository/GoogleDatastoreRepositoryIntegrationTests.java
@@ -16,6 +16,7 @@
 package com.niloen.spring.data.google.datastore.repository;
 
 import com.niloen.spring.data.google.datastore.repository.configuration.EnableGoogleDatastoreRepositories;
+import lombok.Data;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -64,6 +65,7 @@ public class GoogleDatastoreRepositoryIntegrationTests {
 		List<Person> findByFirstname(String firstname);
 	}
 
+	@Data
 	public static class Person implements Serializable {
 
 		@Id
@@ -71,35 +73,6 @@ public class GoogleDatastoreRepositoryIntegrationTests {
 
 		String firstname;
 		String lastname;
-
-		@Override
-		public boolean equals(Object o) {
-			if (this == o) return true;
-			if (o == null || getClass() != o.getClass()) return false;
-
-			Person person = (Person) o;
-
-			if (id != null ? !id.equals(person.id) : person.id != null) return false;
-			if (firstname != null ? !firstname.equals(person.firstname) : person.firstname != null) return false;
-			return lastname != null ? lastname.equals(person.lastname) : person.lastname == null;
-		}
-
-		@Override
-		public int hashCode() {
-			int result = id != null ? id.hashCode() : 0;
-			result = 31 * result + (firstname != null ? firstname.hashCode() : 0);
-			result = 31 * result + (lastname != null ? lastname.hashCode() : 0);
-			return result;
-		}
-
-		@Override
-		public String toString() {
-			return "Person{" +
-					"id='" + id + '\'' +
-					", firstname='" + firstname + '\'' +
-					", lastname='" + lastname + '\'' +
-					'}';
-		}
 	}
 
 	@Before
